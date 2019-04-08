2695d70a6da3d26507d724809cfbf701b0c9a224
Andrew <drew.sotiriou@gmail.com>
2019-04-08 14:19:23 -0400

    Add babel plugin transform runtime and babel runtime so that async/await can be used

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
+            "plugins": [ "@babel/transform-runtime" ]
           },
         },
       },
